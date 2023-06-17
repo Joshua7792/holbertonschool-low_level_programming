11d5328a4022cbb08e51888a6064da0b2e0e52a3
Joshua7792 <j.santiago39@hotmail.com>
2023-06-17 19:47:20 +0000

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - PrintF size
+ * Return: Always 0
+ */
+
+int main(void)
+{
+printf("Size of char: %zu byte(s)\n", sizeof(char));
+printf("Size of short: %zu byte(s)\n", sizeof(short));
+printf("Size of int: %zu byte(s)\n", sizeof(int));
+printf("Size of long: %zu byte(s)\n", sizeof(long));
+
+return (0);
+}
+
