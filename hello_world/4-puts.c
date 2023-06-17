075a0c03de48778bf7561d7232e979af11732e2c
Joshua7792 <j.santiago39@hotmail.com>
2023-06-17 18:31:58 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 /**
- * main - Prints "Programming is like building a multilingual puzzle, followed by a new line.
+ * main -Print Entry point
  *
  * Return: Always 0
  */
