978dcb315680990f0180c96d8e69805d39ba1b20
Joshua7792 <j.santiago39@hotmail.com>
2023-06-21 00:01:51 +0000

    prints alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times alphabet in lowercase
+ * Return: there is no return
+ */
+
+void print_alphabet_x10(void)
+{
+	char x;
+		for (x = 'a' ; x = 'z' ; x++)
+		{
+			_putchar (x);
+		}
+	_putchar ('\n');
+
+}
