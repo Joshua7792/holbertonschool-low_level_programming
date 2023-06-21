8d70d7b4ee5f03ca3f0b2f1195d0ec70e66e001d
Joshua7792 <j.santiago39@hotmail.com>
2023-06-21 00:21:41 +0000

    Prints the alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,18 +1,17 @@
-#include <stdio.h>
 #include "main.h"
-
+#include <stdio.h>
 /**
- * print_alphabet - prints the alphabet
- * Return: there is no return
+ * print_alphabet - function that prints the alphabet
  */
 
 void print_alphabet(void)
 {
 	char x;
-		for (x = 'a' ; x = 'z' ; x++)
-		{
-			_putchar (x);
-		}
-	_putchar ('\n');
 
+	for (x = 'a'; x = 'z'; x++)
+	{
+		_putchar(x);
+	}
+
+	_putchar('\n');
 }
