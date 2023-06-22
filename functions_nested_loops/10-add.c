e516209aa41e11e7c9d9eb3cdbfb0dca226ce473
Joshua7792 <j.santiago39@hotmail.com>
2023-06-22 18:17:39 +0000

    add to integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - add a + b
+ * Return: result
+ * @a: a
+ * @b: b
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+
+}
