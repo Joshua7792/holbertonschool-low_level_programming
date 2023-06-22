41e2c087b93cfbd6d6e370b282967e8fe17b4a8c
Joshua7792 <j.santiago39@hotmail.com>
2023-06-22 01:44:11 +0000

    compute the absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include <inttypes.h>
+
+/**
+ *_abs - computes the abosolute value of an integer
+ *Return: always 0
+ *@n: value of an integer
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (0);
+	}
+}
