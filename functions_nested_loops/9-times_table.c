880c27db267c70765ac078c58ae28e12f228e6bb
Joshua7792 <j.santiago39@hotmail.com>
2023-06-23 00:59:10 +0000

    print 9 times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 	for (row = 0 ; row <= 9 ; row++)
 	{
 		for (col = 0 ; col <= 9 ; col++)
-			_putchar (t + 0);
+			_putchar (t + '0');
 		_putchar (',');
 		_putchar (' ');
 		_putchar ('\n');
