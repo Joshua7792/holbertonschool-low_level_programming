3c2ef76ef373af00023463062c78ab10cc35e471
Joshua7792 <j.santiago39@hotmail.com>
2023-06-23 21:40:33 +0000

    most numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_most_numbers - print most numbers
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = '0' ; x <= '9' ; x++)
+	{
+		if (x != 2 && x != 4)
+		{
+			_putchar(x);
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
