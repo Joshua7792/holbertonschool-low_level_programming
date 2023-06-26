8959de4ccabcb023075bcd3536356ab188edaa95
Joshua7792 <j.santiago39@hotmail.com>
2023-06-26 15:29:08 +0000

    takes a pointer to an int as a parameter

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Takes a pointer to an int as parameter
+ * and uptades the value it points to 98.
+ * @n: a interger
+ */
+void reset_to_98(int *n)
+	n = 98
+	p = 402
+{
+	*n = 98;
+	_putchar (n)
+}
