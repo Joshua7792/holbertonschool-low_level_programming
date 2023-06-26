aaed630c43c2f372806f668d0460a9aea61491ee
Joshua7792 <j.santiago39@hotmail.com>
2023-06-26 17:28:59 +0000

    swaps

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - Swap the value of two intergers
+ * @a: Interger
+ * @b: Interger
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	
+	*a = *b;
+	*b = (temp);
+	
+}
