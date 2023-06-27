662190b9592ad19eca801ebd61a688a7a38b29ac
Joshua7792 <j.santiago39@hotmail.com>
2023-06-27 19:22:25 +0000

    reverse

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void rev_string(char *s) {
+	int length = 0;
+	int beg;
+	int end;
+	char tmp;
+
+	
+	for (; s[length] != '\0'; length++)
+	;
+
+	end = length - 1;
+
+	for (beg = 0; beg < length / 2; beg++)
+	{
+		tmp = s[beg];
+		s[beg] = s[end];
+		s[end] = tmp;
+		end--;
+	}
+}
+
+
