5d879b055d2461ac6deb464615953bb6f2a04836
Joshua7792 <j.santiago39@hotmail.com>
2023-06-28 15:17:35 +0000

    prints half of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *puts_half - prints half of string
+ *@str: pointer
+ */
+
+void puts_half(char *str)
+{
+	int leng = 0;
+	int even, odd;
+
+	for (; str[leng] != ('\0') ; leng++)
+		;
+
+	if ((leng % 2) == 0)
+	{
+		for (even = leng / 2 ; even != '\0' ; even++)
+		{
+			_putchar(str[even]);
+		}
+	}
+	else
+	{
+		for (odd = (leng - 1) / 2 ; odd < leng - 1 ; odd++)
+		{
+			_putchar(str[odd]);
+		}
+	}
+	_putchar('\n');
+
+}
