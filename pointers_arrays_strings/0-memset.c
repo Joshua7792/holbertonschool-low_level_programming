ceba092b19f0f18a7291a15f95ba06898dc547bc
Joshua7792 <j.santiago39@hotmail.com>
2023-07-01 03:35:08 +0000

    memory set

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -13,9 +13,9 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned int i;
 	char *ptr = s;
 
-	for (i = 0; i < n; i++)
+	for (i = 0 ; i < n ; i++)
 	{
-		*ptr = b;
+		ptr[i] = b;
 	}
 
 	return (s);
