0b15549614b36de9dcbf2bea2b454eabb42d67e5
Joshua7792 <j.santiago39@hotmail.com>
2023-07-04 21:46:42 +0000

    returns the  alue of x rasie by the power of y

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -13,11 +13,14 @@ int _pow_recursion(int x, int y)
 	if (y < 0)
 		return (-1);
 
-	else if (x == 0)
+	else if (y == 0)
 		return (1);
+
+	else if (x == 0)
+		return (0);
 
 	else
 	{
-		return (x * int _pow_recursion(y - 1));
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
