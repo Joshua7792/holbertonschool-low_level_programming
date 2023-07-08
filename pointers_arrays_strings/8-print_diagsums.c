65763c3ddbb5c1bd51c91c0a18f09dca14138e3c
Joshua7792 <j.santiago39@hotmail.com>
2023-07-08 00:18:48 -0400

    fixed

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,19 +1,30 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * print_diagsums - prints the sum of two
- * diagonals of a square matrix of interger
+ * print_diagsums - function name
  * @a: pointer
- * @size: number
+ * @size: int var
  */
+
 void print_diagsums(int *a, int size)
 {
-	int i, sum1, sum2;
+	int x = 0;
+	int y = size - 1;
+	int sum1 = 0;
+	int sum2 = 0;
 
-	for (i = 0; i < size; i++)
+	while (x <= (size * size))
 	{
-		sum1 += a[(size * i) + i];
-		sum2 += a[(size * (i + 1)) - (i + 1)];
+		sum1 = sum1 + a[x];
+		x = x + size + 1;
 	}
+
+	while (y < (size * size - 1))
+	{
+		sum2 += a[y];
+		y = y + size - 1;
+	}
+
 	printf("%d, %d\n", sum1, sum2);
 }
