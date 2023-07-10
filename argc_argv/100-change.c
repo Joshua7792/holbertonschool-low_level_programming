ad9ecf1ff25d2fce49623d702a730e4eea06821a
Joshua7792 <j.santiago39@hotmail.com>
2023-07-10 13:52:02 +0000

    fixed

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -9,37 +9,38 @@
  */
 int main(int argc, char *argv[])
 {
-    int cents, coins;
-    if (argc != 2)
-    {
-        printf("Error/n");
-        return (1);
-    }
-    cents = atoi(argv[1]);
+	int cents, coins;
 
-    for (coins = 0; coins > 0; coins++)
-    {
-        if ((cents - 25) >= 0)
-        {
-            cents -= 25;
-            continue;
-        }
-        if ((cents - 10) >= 0)
-        {
-            cents -= 10;
-            continue;
-        }
-        if ((cents - 5) >= 0)
-        {
-            cents -= 5;
-            continue;
-        }
-        if ((cents - 2) >= 0)
-        {
-            cents -= 2;
-            continue;
-        }
-    }
-    printf("%d/n", coins);
-    return (0);
+	if (argc != 2)
+	{
+		printf("Error/n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+
+	for (coins = 0; coins > 0; coins++)
+	{
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+	}
+	printf("%d/n", coins);
+	return (0);
 }
