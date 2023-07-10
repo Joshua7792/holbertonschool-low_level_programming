67cd8195c14b692be98b989071a6c9fe0faa1891
Joshua7792 <j.santiago39@hotmail.com>
2023-07-10 19:35:14 -0400

    fixed

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,21 +14,25 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
+/** Check if the size is 0. If it is, return NULL. */
 	if (size == 0)
 	{
 		return (NULL);
 	}
-
+/** Allocate a memory block for the array. */
 	ptr = malloc(sizeof(char) * size);
 
+/** Check if the memory allocation was successful.If it wasn't, return NULL*/
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
 
+/** Iterate over the array and fill it with the specified character. */
 	for (i = 0 ; i < size ; i++)
 	{
 		ptr[i] = c;
 	}
+/** Return a pointer to the newly created array. */
 	return (ptr);
 }
