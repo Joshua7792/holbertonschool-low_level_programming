7675941597c5560a091d96099b07398afa3e8f77
Joshua7792 <j.santiago39@hotmail.com>
2023-07-17 14:40:51 -0400

    Define a new type struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,19 +1,24 @@
+#ifndef DOG_T
+#define DOG_T
+
 /**
  * struct dog - struct for dog
  * @name: name of dog
  * @age: age of dog
  * @owner: name of owner
- * Prototypes
  */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}; typedef struct dog dog
+};
+/* dog_t - typedef for structure */
+typedef struct dog dog_t;
 
 /* prototypes use in this header file */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+#endif
