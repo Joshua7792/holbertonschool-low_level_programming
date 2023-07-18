b8228b2f60ee83914be72ec566ca4ecea5315e33
Joshua7792 <j.santiago39@hotmail.com>
2023-07-17 20:12:18 -0400

    free dog

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_dog - free function
+ * @d: parameter
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		free(d);
+	}
+}
