648425bc5a45a4b6c1185b315724d42dddd7dcb3
Joshua7792 <j.santiago39@hotmail.com>
2023-07-18 21:37:55 -0400

    fixed

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -18,13 +18,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
-
 	/*Check if the size is less than or equal to 0. If it is, return -1.*/
 	if (size <= 0)
 	{
 		return (-1);
 	}
-
 	/**
 	 * comparing each element to the comparison function.
 	 * If the comparison function is not a 0, return the index of the element.
