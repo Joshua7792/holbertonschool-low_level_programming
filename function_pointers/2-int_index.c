f619d2497fc3bf71781591780233dcc29aee9428
Joshua7792 <j.santiago39@hotmail.com>
2023-07-18 22:02:26 -0400

    fixed

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,20 +13,17 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+	/*Check if the array and comparison function are NULL. */
+	if (array == NULL && cmp == NULL)
+	{
+		return (-1);
+	}
 	/*Check if the size is less than or equal to 0. If it is, return -1.*/
 	if (size <= 0)
 	{
 		return (-1);
 	}
-	/*Check if the array and comparison function are NULL. */
-	if (array == NULL && cmp == NULL)
-	{
-		return (-1);
-	}	
-	/**
-	 * comparing each element to the comparison function.
-	 * If the comparison function is not a 0, return the index of the element.
-	 */
+	/* If the comparison function is not a 0, return the index of the element.*/
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
