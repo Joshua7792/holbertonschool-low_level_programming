65d7785485a6e40caf20085764d2289529db1b2f
Joshua7792 <j.santiago39@hotmail.com>
2023-07-18 21:09:28 -0400

    pon un if

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -12,17 +12,28 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+  /*Check if the array and comparison function are NULL. If they are, return -1.*/
+  if (array == NULL && cmp == NULL)
+  {
+    return (-1);
+  }
 
-	if (array == NULL && cmp == NULL)
-		return (-1);
+  /*Check if the size is less than or equal to 0. If it is, return -1.*/
+  if (size <= 0)
+  {
+    return (-1);
+  }
 
-	if (size <= 0)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+  /** Iterate through the array, comparing each element to the comparison function.
+  *If the comparison function is not a 0, return the index of the element. 
+  */
+  for (i = 0; i < size; i++) {
+    if (cmp(array[i]) != 0)
 	{
-		if (cmp(array[i]))
-		return (i);
-	}
-	return (-1);
+      return (i);
+    }
+  }
+
+  /* If no match was found, return -1.*/
+  return (-1);
 }
