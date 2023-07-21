05ebebfb6747552a87661a5988e81cc56fc73b0b
Joshua7792 <j.santiago39@hotmail.com>
2023-07-21 13:46:08 -0400

    fixed

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -11,8 +11,7 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] =
-	{
+	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
@@ -24,7 +23,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 5)
 	{
-		if (strcmp(s,ops[i].op) == 0)
+		if (strcmp(s, ops[i].op) == 0)
 		{
 			return (ops[i].f);
 		}
