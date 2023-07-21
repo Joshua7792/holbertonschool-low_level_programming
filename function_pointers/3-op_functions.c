2e69753f56ed9f475b5d845d8b7aa277b51c22ee
Joshua7792 <j.santiago39@hotmail.com>
2023-07-21 13:29:28 -0400

    task 3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * op_add - function to sum
+ * @a: number one
+ * @b: number two
+ * Return: the result of the equation
+ */
+
+int op_add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+ * op_sub - function to sub
+ * @a: number one
+ * @b: number two
+ * Return: the result of the equation
+ */
+
+int op_sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+ * op_mul - function to mul
+ * @a: number one
+ * @b: number two
+ * Return: the result of the equation
+ */
+int op_mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * op_div - function to div
+ * @a: number one
+ * @b: number two
+ * Return: the result of the equation
+ */
+int op_div(int a, int b)
+{
+    return (a / b);
+}
+
+/**
+ * op_mod - function to mod
+ * @a: number one
+ * @b: number two
+ * Return: the result of the equation
+ */
+int op_mod(int a, int b)
+{
+    return (a % b);
+}
