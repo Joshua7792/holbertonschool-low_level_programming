b1ca17a107e98a8321e8e770fb2ce54585bc9c2b
Joshua7792 <j.santiago39@hotmail.com>
2023-07-21 22:56:45 -0400

    fixed

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -11,39 +11,65 @@ void print_all(const char * const format, ...)
 	int a = 0, b = 0;
 	char *p = NULL;
 
+	/* Loop through the format string, one character at a time */
 	va_start(all, format);
 	while (format && format[b])
 		b++;
 	while (format && format[a])
 	{
+		/* Get the next argument from the variable argument list */
 		switch (format[a])
 		{
+			/* Print a character */
 			case 'c':
 				printf("%c", va_arg(all, int));
 				break;
+
+			/* Print an integer */
 			case 'i':
 				printf("%d", va_arg(all, int));
 				break;
+
+			/* Print a float */
 			case 'f':
 				printf("%f", va_arg(all, double));
 				break;
+
+			/* Print a string */
 			case 's':
+				/* Get the string from the variable argument list */
 				p = va_arg(all, char *);
+
+				/* Check if the string is NULL */
 				if (p == NULL)
 				{
+					/* Print "(nil)" */
 					printf("(nil)");
 					break;
 				}
+
+				/* Print the string */
 				printf("%s", p);
 				break;
+
+			/* Default case */
 			default:
+				/* Do nothing */
 				a++;
 				continue;
 		}
+
+		/* If this is not the last argument, print a comma */
 		if (a != b - 1)
 			printf(", ");
+
+		/* Increment the counter */
 		a++;
 	}
-	printf("\n");
+
+	/* Close the variable argument list */
 	va_end(all);
+
+	/* Print a newline */
+	printf("\n");
 }
