aa97ef242ebe576d062c6398e5adc06c5200365e
Joshua7792 <j.santiago39@hotmail.com>
2023-07-31 15:27:26 -0400

    function that prints all the elements of a list.

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,7 +1,7 @@
 include "lists.h"
 
 /**
- * print_list - Prints all the elements of a list_t list.
+ * print_list - Prints all the elements of a list_t list
  * @h: Pointer to the first element of the list.
  * Return: The number of nodes in the list.
  */
