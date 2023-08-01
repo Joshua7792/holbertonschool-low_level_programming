728206f6ba2679c440a284796e2395073201002b
Joshua7792 <j.santiago39@hotmail.com>
2023-08-01 14:10:28 -0400

    function that add a new node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,19 +11,20 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
+	/* Allocate a new node. */
 	new_node = malloc(sizeof(list_t));
 	if (str == NULL || new_node == NULL)
+	/* The malloc() call failed */
 		return (NULL);
 
+	/* Copy the string & Calculate the length of the new node */
 	new_node->str = strdup(str);
+	new_node->len = strlen(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
-	/* Calculate the length of the new string */
-	new_node->len = strlen(str);
 
 	/* Update the next pointer of the new node to point to the current head */
 	new_node->next = *head;
