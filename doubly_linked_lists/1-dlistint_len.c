f9d1618c8e159582fab6ae58248d877ffe93ce08
Joshua7792 <j.santiago39@hotmail.com>
2023-08-01 22:51:30 -0400

    Number of elements

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,19 +1,26 @@
 #include "lists.h"
 
 /**
- * dlistint_len - len of a list
- * @h: pointer
- * Return: count
+ * dlistint_len - Returns the number of elements in a dlistint_t list.
+ * @h: Pointer to the head of the list.
+ * Return: Number of elements (length) of the list.
  */
 
 size_t dlistint_len(const dlistint_t *h)
 {
-	int count = 0;
+    /* Initialize a count variable to keep track of the number of elements */
+    int count = 0;
 
-	while (h != NULL)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
+    /* Traverse the list while there are still nodes to process */
+    while (h != NULL)
+    {
+        /* Increment the count for each node encountered */
+        count++;
+
+        /* Move to the next node in the list */
+        h = h->next;
+    }
+
+    /* Return the final count, which represents the length of the list */
+    return (count);
 }
