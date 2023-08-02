0ada1ffc1fbe97fae3f6fe58f41d0e9fdac14f24
Joshua7792 <j.santiago39@hotmail.com>
2023-08-01 22:50:43 -0400

    return numbers of elements

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,22 +1,29 @@
 #include "lists.h"
 
 /**
- * print_dlistint - print the list
- * @h: int
- * Return: the number of nodes
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ * @h: Pointer to the head of the list.
+ * Return: Number of nodes (elements) in the list.
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count = 0;
+    /* Initialize a count variable to keep track of the number of nodes */
+    int count = 0;
 
-	while (h != NULL)
-	{
-		{
-			printf("%d\n", h->n);
-		}
-		count++;
-		h = h->next;
-	}
-	return (count);
+    /* Traverse the list while there are still nodes to process */
+    while (h != NULL)
+    {
+        /* Print the value of the current node */
+        printf("%d\n", h->n);
+
+        /* Increment the count for each node encountered */
+        count++;
+
+        /* Move to the next node in the list */
+        h = h->next;
+    }
+
+    /* Return the final count, which represents the number of nodes printed */
+    return (count);
 }
