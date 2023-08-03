befd7ddcc7ae9a34a37ebf705503f401a848e204
Joshua7792 <j.santiago39@hotmail.com>
2023-08-02 22:23:53 -0400

    Returns the sum of all the data

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -8,16 +8,16 @@
 
 int sum_dlistint(dlistint_t *head)
 {
-    /* Initialize a variable to keep track of the sum */
-    int sum = 0;
-    /* Go through the list and add the data (n) of each node to the sum */
-    while (head != NULL)
-    {
-        /* Add the value of the current node's data (n) to the sum */
-        sum += head->n;
-        /* Move to the next node in the list */
-        head = head->next;
-    }
-    /* Return the sum of all the data (n) or 0 if the list is empty */
-    return (sum);
+	/* Initialize a variable to keep track of the sum */
+	int sum = 0;
+	/* Go through the list and add the data (n) of each node to the sum */
+	while (head != NULL)
+	{
+		/* Add the value of the current node's data (n) to the sum */
+		sum += head->n;
+		/* Move to the next node in the list */
+		head = head->next;
+	}
+	/* Return the sum of all the data (n) or 0 if the list is empty */
+	return (sum);
 }
