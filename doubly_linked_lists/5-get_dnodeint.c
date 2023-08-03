bfda2f3b1789432dc82501bb1763da409adb50d0
Joshua7792 <j.santiago39@hotmail.com>
2023-08-02 21:49:56 -0400

    returns the nth node

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -11,11 +11,12 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	if (head == NULL)
-		return (NULL);
-
 	while (i != index)
 	{
+		if (head == NULL)
+		{
+			return (NULL);
+		}
 		head = head->next;
 		i++;
 	}
