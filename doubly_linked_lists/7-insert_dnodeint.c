8cd10af5b887c35c6396ce6eb4e6949db3499f3a
Joshua7792 <j.santiago39@hotmail.com>
2023-08-04 12:55:33 -0400

    Inserts a new node

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,11 +11,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *tmp = *h;
 	unsigned int i;
-
+	
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	for (i = 0; i < idx; i++)
+	for (i = 0;  i < idx - 1; i++)
 	{
 		tmp = tmp->next;
 		if (tmp == NULL)
