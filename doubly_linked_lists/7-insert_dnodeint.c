f63690265ad69c724d13f710aa9dd02c28fbcfa9
Joshua7792 <j.santiago39@hotmail.com>
2023-08-04 11:24:50 -0400

    Inserts a new node

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -9,43 +9,32 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp, *new_node;
+	dlistint_t *new_node, *tmp = *h;
 	unsigned int i;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (i = 0; i < idx; i++)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+		{
+			return (NULL);
+		}
+	}
+
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
-	if (idx == 0)
-	{
-		new_node->next = *h;
-		new_node->prev = NULL;
-		if (*h != NULL)
-			(*h)->prev = new_node;
-		*h = new_node;
-		return (new_node);
-	}
-
-	tmp = *h;
-	for (i = 0; i < idx; i++)
-	{
-		if (tmp == NULL)
-		{
-			free(new_node);
-			return (NULL);
-		}
-		tmp = tmp->next;
-	}
-	if (tmp == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->next = tmp->next;
 	new_node->prev = tmp;
-	if (tmp->next != NULL)
-		tmp->next->prev = new_node;
+	new_node->next = tmp->next;
+	tmp->next->prev = new_node;
 	tmp->next = new_node;
 
 	return (new_node);
