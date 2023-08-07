37d94d2e496e6a0c61bc3785922193c1b08abd0c
Joshua7792 <j.santiago39@hotmail.com>
2023-08-07 14:12:04 -0400

    function that reads a text file and prints

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,33 +1,33 @@
 #include "main.h"
 
 /**
- * read_textfile - function that reads a text file 
+ * read_textfile - function that reads a text file
  * @filename: file name
  * @letters: how many letter will be read
  * Return: actual number of letters it could read and print
-*/
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-   	/* Declare variables */
+	/* Declare variables */
 	int f, n;
 	char *buf = malloc(sizeof(char) * letters);
 
-    if (buf == NULL)
-    return (0);
+	if (buf == NULL)
+		return (0);
 
-    if (filename == NULL)
-    return (0);
+	if (filename == NULL)
+		return (0);
 
-    f = open(filename, O_RDONLY);
-    if (f == -1)
-    return (0);
+	f = open(filename, O_RDONLY);
+	if (f == -1)
+		return (0);
 
-    n = read(f, buf, letters);
+	n = read(f, buf, letters);
 
-    write (1, buf, n);
-    free(buf);
-    close(f);
+	write(1, buf, n);
+	free(buf);
+	close(f);
 
-    return (n);
+	return (n);
 }
