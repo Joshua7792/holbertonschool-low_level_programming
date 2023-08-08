7608235ac819d841e730765a4aba05c0fb79d00c
Joshua7792 <j.santiago39@hotmail.com>
2023-08-07 23:54:59 -0400

    function that gives you the index of a key

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 /**
- * key_index -function that gives you the index of a key
+ * key_index - function that gives you the index of a key
  *
  * @key: is the key
  * @size: is the size of the array of the hash table
@@ -10,8 +10,8 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    if (key == NULL || size == 0)
-        return (0);
+	if (key == NULL || size == 0)
+		return (0);
 
-    return (hash_djb2(key) % size);
+	return (hash_djb2(key) % size);
 }
