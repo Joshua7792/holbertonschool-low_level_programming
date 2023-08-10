fa58555a7fe080e8df1f581c58a2bff6aff0698f
Joshua7792 <j.santiago39@hotmail.com>
2023-08-10 14:31:04 -0400

    Write a function that prints a hash table.

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: pointer to the hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *table;
+	char *sep;
+
+	/* Check if the hash table is NULL */
+	if (ht == NULL)
+		return;
+
+	/* Initialize separator and print opening brace */
+	sep = "";
+	printf("{");
+
+	/* Iterate through the array and print key-value pairs */
+	for (i = 0; i < ht->size; i++)
+	{
+		table = ht->array[i];
+		while (table != NULL)
+		{
+			/* Print key-value pair with appropriate formatting */
+			printf("%s'%s': '%s", sep, table->key, table->value);
+			sep = ", ";
+			table = table->next;
+		}
+	}
+
+	/* Print closing brace and newline character */
+	printf("}\n");
+}
