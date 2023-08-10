ca7aff44811c6341367df5ccf7f965d4912dfa45
Joshua7792 <j.santiago39@hotmail.com>
2023-08-10 14:42:34 -0400

    prints a hash table

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -4,25 +4,32 @@
  * hash_table_print - prints a hash table
  * @ht: pointer to the hash table
  */
+
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *table;
 	char *sep;
 
+	/* Check if the hash table is NULL */
 	if (ht == NULL)
 		return;
-	printf("{");
+	printf("{"); /* print opening brace */
+
+	/* Initialize separator */
 	sep = "";
+	/* Iterate through the array and print key-value pairs */
 	for (i = 0; i < ht->size; i++)
 	{
 		table = ht->array[i];
 		while (table != NULL)
 		{
+			/* Print key-value pair with appropriate formatting */
 			printf("%s'%s': '%s'", sep, table->key, table->value);
 			sep = ", ";
 			table = table->next;
 		}
 	}
+	/* Print closing brace and newline character */
 	printf("}\n");
 }
